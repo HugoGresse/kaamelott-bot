a3cce4c1bb28e10aea5b26c81aec30e955ff5a8c
Hugo Gresse <hugo.gresse@gmail.com>
2020-04-16 20:34:05 +0200

    Add getAppToken

--- functions/src/https/utils/getAppToken.ts
+++ functions/src/https/utils/getAppToken.ts
@@ -1,11 +1,11 @@
 import {db} from './initFirebase'
 
-export const getWebhookUrl = async (teamId: string) => {
+export const getAppToken = async (teamId: string): Promise<string | null> => {
     const docSnapshot = await db.collection("installations").doc(teamId).get()
 
     if (docSnapshot.exists) {
         // @ts-ignore
-        return docSnapshot.data().webhook.url
+        return docSnapshot.data().token
     }
     return null
 }

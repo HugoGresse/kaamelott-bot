6899a2c7c6f7812756e8f618b1da25e49518b888
Hugo Gresse <hugo.gresse@gmail.com>
2020-04-18 13:42:51 +0200

    Add useless method

--- functions/src/https/utils/findBestSoundsMatch.ts
+++ functions/src/https/utils/findBestSoundsMatch.ts
@@ -13,3 +13,6 @@ export const findBestSoundsMatch = async (inputText: string): Promise<Sound[]> =
 
     return sounds.filter(sound => sound.title.trim().toLowerCase().includes(inputText))
 }
+
+export const getFileUrl = (sound: Sound): string => `https://raw.githubusercontent.com/2ec0b4/kaamelott-soundboard/master/sounds/${sound.file}`
+export const getFileGUID = (sound: Sound): string => `kaamelott-soundboard/${sound.file}`

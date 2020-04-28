7f2a262a28734787cce29578a601738a15448fbd
Hugo Gresse <hugo.gresse@gmail.com>
2020-04-28 18:17:44 +0200

    Add readme & fix ci build

--- src/pages/SlackSuccess.js
+++ src/pages/SlackSuccess.js
@@ -7,16 +7,19 @@ const SlackSuccess = () => {
         textAlign: "center",
         minHeight: '100vh',
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center"
     }}>
 
         <h3>
-            Slack app successfully installed <span role="image">🎉</span>
+            Slack app successfully installed
         </h3>
-
+        <br/>
+        <p style={{fontSize: 100, marginTop: 0}}>
+            <span role="img" aria-label="img">🎉</span>
+        </p>
     </div>
-
 }
 
 export default SlackSuccess

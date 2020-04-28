2d7b7e6d612f76d99647d7cae57c0f6787ce9517
Hugo Gresse <hugo.gresse@gmail.com>
2020-04-28 18:20:09 +0200

    Update website root

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -14,8 +14,9 @@ const Root = () => {
     }}>
 
         To install the bot, use the button bellow
-
-        <a href="https://slack.com/oauth/v2/authorize?client_id=89232678994.1064034348338&scope=chat:write,chat:write.public,commands,files:write,links:read">
+        <br/>
+        <br/>
+        <a href="https://slack.com/oauth/v2/authorize?client_id=89232678994.1064034348338&scope=commands">
             <img
                 alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png"
                 srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/>
@@ -24,7 +25,7 @@ const Root = () => {
         <a href="https://github.com/HugoGresse/kaamelott-bot"
            style={{
                marginTop: 20
-           }}>Github</a>
+           }}>More info on Github</a>
 
     </div>
 

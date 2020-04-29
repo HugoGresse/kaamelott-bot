c910e673cb06a1128284f93223832aced1ef5c65
Hugo Gresse <hugo.gresse@gmail.com>
2020-04-29 12:31:22 +0200

    Add username to answer & try to respond as fast as possible to the interactivity command

--- functions/src/https/slackInteractivity.ts
+++ functions/src/https/slackInteractivity.ts
@@ -8,6 +8,10 @@ import {twitterUpload} from '../twitter-cron/twitterCron'
 const TYPE_BLOCK_ACTIONS = "block_actions"
 
 export const slackInteractivity = functions.https.onRequest(async (request, response) => {
+    response
+        .status(200)
+        .send("✅  Ça arrive !")
+
     const token = await verifySlackPostRequest(request, response)
     if (!token) {
         return
@@ -21,24 +25,22 @@ export const slackInteractivity = functions.https.onRequest(async (request, resp
 
     if (type !== TYPE_BLOCK_ACTIONS) {
         console.log("Received a not implemented type " + type)
-        return response.status(501).send('Type not implemented')
+        return callResponseUrl('Type not implemented', responseUrl, 501)
     }
     if (!channelId) {
         console.log("No channel id")
-        return response.status(400).send('Missing channel id')
+        return callResponseUrl('Missing channel id', responseUrl, 400)
     }
     if (!responseUrl) {
         console.log("No response_url")
-        return response.status(400).send('Missing response_url')
+        return callResponseUrl('Missing response_url', responseUrl, 400)
     }
     if (!selectedFile) {
         console.log("No selected file or action not supported")
-        return response.status(400).send('Missing supported action')
+        return callResponseUrl('Missing supported action', responseUrl, 400)
     }
 
-    response
-        .status(200)
-        .send("✅  Ça arrive !")
+    await callResponseUrl("✅  Ça arrive !", responseUrl)
 
     const tweetLink = await getTweetLink(selectedFile)
 
@@ -48,7 +50,7 @@ export const slackInteractivity = functions.https.onRequest(async (request, resp
             delete_original: true,
             replace_original: false,
             response_type: 'in_channel',
-            text: tweetLink,
+            text: `Posté par <@${payload.user.id}> \r\n ${tweetLink}`,
             unfurl_media: true,
             unfurl_link: true,
         })
@@ -69,4 +71,14 @@ const getTweetLink = async (selectedFile: string): Promise<TweetLink> => {
 
 const constructTweetLink = (sound: Sound): TweetLink => {
     return `https://twitter.com/${functions.config().twitter.account_name}/status/${sound.tweetId}`
+}
+
+const callResponseUrl = (text: string, url: string, code?: number): Promise<any> => {
+    const msg = code ? `Erreur ${code}: ${text}` : text
+    return fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            text: msg,
+        })
+    })
 }
